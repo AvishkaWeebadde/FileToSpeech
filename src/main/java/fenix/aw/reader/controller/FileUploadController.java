f7aaffe0233694bb45691816f8872db73c73d7ee
AvishkaWeebadde <senithweebadde@gmail.com>
2024-10-06 21:12:58 +0530

    Adding functionality to controller level

--- src/main/java/fenix/aw/reader/controller/FileUploadController.java
+++ src/main/java/fenix/aw/reader/controller/FileUploadController.java
@@ -1,23 +1,27 @@
 package fenix.aw.reader.controller;
 
+import fenix.aw.reader.Exception.StorageFileNotFoundException;
 import fenix.aw.reader.service.IStorageService;
-import fenix.aw.reader.service.impl.StorageService;
-import org.hibernate.cache.spi.support.StorageAccess;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.stream.Collectors;
 
-@Controller
+@RestController
 public class FileUploadController {
 
     private final IStorageService storageService;
 
     @Autowired
-    public FileUploadController(IStorageService storageService) {
+    public FileUploadController(IStorageService storageService)
+    {
         this.storageService = storageService;
     }
 
@@ -30,5 +34,39 @@ public class FileUploadController {
                 .collect(Collectors.toList()));
 
         return "uploadForm";
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename)
+    {
+        Resource file = storageService.loadAsResource(filename);
+
+        if(file==null)
+        {
+            return ResponseEntity
+                    .notFound()
+                    .build();
+        }
+
+        return ResponseEntity
+                .ok()
+                .header(
+                HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"")
+                .body(file);
+    }
+
+    @PostMapping("/")
+    public String handleFileUpload(@RequestParam("file")MultipartFile file, RedirectAttributes redirectAttributes)
+    {
+        storageService.store(file);
+        redirectAttributes.addFlashAttribute("message",
+                "Successfully uploaded" + file.getOriginalFilename() + "!");
+        return "redirect/";
+    }
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
     }
 }
